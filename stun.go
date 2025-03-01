11ff1370163e515a0894c78d5cba3d080b738b84
Justine <paralejasjustine15@gmail.com>
2025-03-01 16:57:11 +0800

    fix

--- stun.go
+++ stun.go
@@ -7,52 +7,51 @@ import (
 	"github.com/pion/stun/v3"
 )
 
-func StartSTUN() {
-	conn, err := net.ListenPacket("udp", ":3478")
+func startListener(network, address string) {
+	conn, err := net.ListenPacket(network, address)
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
 
-	fmt.Println("STUN server is listening on port 3478...")
-
+	fmt.Printf("STUN server is listening on %s (%s)...\n", address, network)
 	buffer := make([]byte, 1500)
+
 	for {
 		n, addr, err := conn.ReadFrom(buffer)
 		if err != nil {
-			log.Errorf("Read error: %v", err)
-			continue
-		}
-
-		udpAddr, ok := addr.(*net.UDPAddr)
-		if !ok {
-			log.Errorf("Failed to cast net.Addr to *net.UDPAddr")
+			fmt.Printf("Read error: %v\n", err)
 			continue
 		}
 
 		message := &stun.Message{}
 		if err := message.UnmarshalBinary(buffer[:n]); err != nil {
-			log.Errorf("Invalid STUN message from %v: %v", addr, err)
+			fmt.Printf("Invalid STUN message from %v: %v\n", addr, err)
 			continue
 		}
 
 		if message.Type.Method == stun.MethodBinding {
-			log.Infof("Received STUN Binding request from %v\n", addr)
+			fmt.Printf("Received STUN Binding request from %v\n", addr)
 
 			response := stun.MustBuild(
 				stun.TransactionID,
 				stun.BindingSuccess,
-				&stun.MappedAddress{
-					IP:   udpAddr.IP,
-					Port: udpAddr.Port,
+				&stun.XORMappedAddress{
+					IP:   addr.(*net.UDPAddr).IP,
+					Port: addr.(*net.UDPAddr).Port,
 				},
 			)
 
 			if _, err := conn.WriteTo(response.Raw, addr); err != nil {
-				log.Errorf("Failed to send STUN response: %v", err)
+				fmt.Printf("Failed to send STUN response: %v\n", err)
 			} else {
-				log.Infof("Sent STUN Binding response to %v\n", addr)
+				fmt.Printf("Sent STUN Binding response to %v\n", addr)
 			}
 		}
 	}
+}
+
+func StartSTUN() {
+	go startListener("udp4", "0.0.0.0:3478") // IPv4
+	go startListener("udp6", "[::]:3478")    // IPv6
 }
