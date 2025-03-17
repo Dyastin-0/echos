91a414fbf0ec87dc8ccfad1ac5c3f04992fac8b6
Justine <paralejasjustine15@gmail.com>
2025-03-18 00:52:02 +0800

    feat: add ws message

--- types.go
+++ types.go
@@ -11,6 +11,7 @@ type websocketMessage struct {
 	ID         string `json:"id,omitempty"`
 	Event      string `json:"event,omitempty"`
 	Data       string `json:"data,omitempty"`
+	AdData     string `json:"adData,omitempty"`
 	Type       string `json:"type,omitempty"`
 	State      bool   `json:"state"`
 	Target     string `json:"target,omitempty"`
