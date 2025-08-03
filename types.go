3a801befbaf4ea03ede64282fc3a2af48b8ec62f
Justine Paralejas <paralejasjustine15@gmail.com>
2025-08-03 22:47:27 +0800

    fix: add fields

--- types.go
+++ types.go
@@ -7,14 +7,15 @@ import (
 type authFunc func(r *http.Request) bool
 
 type websocketMessage struct {
-	Name       string `json:"name,omitempty"`
-	ID         string `json:"id,omitempty"`
-	Event      string `json:"event,omitempty"`
-	Data       string `json:"data,omitempty"`
-	AdData     string `json:"adData,omitempty"`
-	Type       string `json:"type,omitempty"`
-	State      bool   `json:"state"`
-	Target     string `json:"target,omitempty"`
-	AudioState bool   `json:"audioState"`
-	VideoState bool   `json:"videoState"`
+	Name           string `json:"name,omitempty"`
+	ID             string `json:"id,omitempty"`
+	Event          string `json:"event,omitempty"`
+	Data           string `json:"data,omitempty"`
+	StreamID       string `json:"streamId,omitempty"`
+	ScreenStreamID string `json:"screenStreamId,omitempty"`
+	Type           string `json:"type,omitempty"`
+	State          bool   `json:"state"`
+	Target         string `json:"target,omitempty"`
+	AudioState     bool   `json:"audioState"`
+	VideoState     bool   `json:"videoState"`
 }
